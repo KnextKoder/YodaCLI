68fc553f90ae6266cfca5e2df914aba2b7958f02
knextkoder <knextkoderklan@gmail.com>
2025-07-08 23:34:34 +0100

    deleted dist folder and set gitignore to !commit again

--- dist/components/WelcomeScreen.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface WelcomeScreenProps {
-    topic?: string;
-}
-declare const WelcomeScreen: React.FC<WelcomeScreenProps>;
-export default WelcomeScreen;
-//# sourceMappingURL=WelcomeScreen.d.ts.map
